277e71f25dce14f100ca32d7a3038df3e112f36d
Kacper <k.trzepiecinski@gmail.com>
2022-09-25 11:51:22 +0200

    delete app.json and update tests.py file

--- tests.py
+++ tests.py
@@ -15,10 +15,9 @@ class TestTraffitBot(unittest.TestCase):
         print(f'tearing down clas...{cls.__name__}')
         del cls.bot
 
-    # def test_1_logging(self):
-    #     self.assertEqual(self.bot.login, "Success")
-    def test_1_smt(self):
-        self.assertEqual(4+2, 6)
+    def test_1_logging(self):
+        self.assertEqual(self.bot.login, "Success")
+
 
 
 
