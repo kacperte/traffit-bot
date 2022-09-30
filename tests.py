981649d239c2179ab3b57f675ce4797b1e82fc8d
Kacper <k.trzepiecinski@gmail.com>
2022-10-01 00:32:03 +0200

    add new feutre

--- tests.py
+++ tests.py
@@ -44,16 +44,68 @@ class TestTraffitBot(TraffitBot):
                 EC.presence_of_all_elements_located((By.CLASS_NAME, "sc-jOhDuK"))
             )
             if elements:
+                self.driver.delete_all_cookies()
                 print("#Kanbas Class Name__Success__")
             else:
                 print("#Kanbas Class Name__Problem__")
         except Exception as e:
             print(f"{e} #Kanbas Class Name__Error__")
 
+    def test_candidate_card_class_name(self):
+        try:
+            self.login_to_traffit()
+            self.driver.get(f"https://hsswork.traffit.com/#/recruitments/recruitment/{self.random_project_id}")
+
+            elements = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "sc-jIAOiI"))
+            )
+            if elements:
+                self.driver.delete_all_cookies()
+                print("#Candidate Card Class Name__Success__")
+            else:
+                print("#Candidate Card Name__Problem__")
+        except Exception as e:
+            print(f"{e} #Candidate Card Name__Error__")
+
+    def test_candidate_name_class_name(self):
+        try:
+            self.login_to_traffit()
+            self.driver.get(f"https://hsswork.traffit.com/#/recruitments/recruitment/{self.random_project_id}")
+
+            elements = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "sc-bgrGEg"))
+            )
+            if elements:
+                self.driver.delete_all_cookies()
+                print("#Candidate Name Class Name__Success__")
+            else:
+                print("#Candidate Name Name__Problem__")
+        except Exception as e:
+            print(f"{e} #Candidate Name Name__Error__")
+
+    def test_candidate_days_class_name(self):
+        try:
+            self.login_to_traffit()
+            self.driver.get(f"https://hsswork.traffit.com/#/recruitments/recruitment/{self.random_project_id}")
+
+            elements = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "sc-erPKsr"))
+            )
+            if elements:
+                self.driver.delete_all_cookies()
+                print("#Candidate Days Class Name__Success__")
+            else:
+                print("#Candidate Days Name__Problem__")
+        except Exception as e:
+            print(f"{e} #Candidate Days Name__Error__")
+
     def main(self):
         self.test_login_to_traffit()
         self.test_get_id_of_all_actvie_project()
         self.test_kanbans_class_name()
+        self.test_candidate_card_class_name()
+        self.test_candidate_name_class_name()
+        self.test_candidate_days_class_name()
 
 
 if __name__ == '__main__':
