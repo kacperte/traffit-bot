1ef8d16bb00137bb8e105a260688cbcd2e36893b
Kacper <k.trzepiecinski@gmail.com>
2023-02-08 12:02:50 +0100

    change andidate_name_class_name and candidate_days_class_name to proper value

--- tests.py
+++ tests.py
@@ -93,7 +93,7 @@ class TestTraffitBot(TraffitBot):
             )
 
             elements = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "sc-gmSHEY"))
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "sc-dwLEzm"))
             )
             if elements:
                 self.driver.delete_all_cookies()
